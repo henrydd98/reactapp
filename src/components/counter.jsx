18aaace0b6cb5db926a5d20fd326cbffe28554bc
Henry Dang <henrydd.98@gmail.com>
2019-02-11 10:50:31 -0500

    Add Counter module

---
+++ src/components/counter.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold",
+    color: "red"
+  };
+
+  //onClick event handler that increments count
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  //returns React element
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {this.renderTags()}
+      </React.Fragment>
+    );
+  }
+
+  renderTags() {
+    if (this.state.tags.length === 0)
+      return <p style={this.styles}>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  //returns a bootstrap class depending if the count variable is 0 or not
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
